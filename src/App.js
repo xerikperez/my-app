8a879186f2d83ac2fe7c12c39c44f0e424aa9fe2
kodierer935 <xerikperez@gmail.com>
2025-02-19 14:29:32 -0300

    analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Analytics } from "@vercel/analytics/react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
